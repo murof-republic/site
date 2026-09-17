aaa140b69aa62711f61ab15d99b15f84a2a5606e
Trigger <triggerww@outlook.com>
2026-09-17 16:29:36 -0300

    adição das coleções apoiadores e artes

---
+++ src/firebase/artes.ts
@@ -0,0 +1,18 @@
+import { collection, getDocs } from 'firebase/firestore'
+import { db } from './firebase'
+
+export interface Arte {
+    id: string
+    autor: string
+    id_autor: string
+    url: string
+}
+
+export async function buscarArtes(): Promise<Arte[]> {
+    const snapshot = await getDocs(collection(db, 'artes'))
+
+    return snapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data()
+    })) as Arte[]
+}
