f0ab95190409df9de5ac3ad01cd7106e8609fbf0
Trigger <triggerww@outlook.com>
2026-09-17 09:04:55 -0300

    commit inicial

---
+++ src/services/firebase.ts
@@ -0,0 +1,15 @@
+import { initializeApp } from 'firebase/app'
+import { getFirestore } from 'firebase/firestore'
+
+const firebaseConfig = {
+    apiKey: 'SUA_API_KEY',
+    authDomain: 'SEU_AUTH_DOMAIN',
+    projectId: 'SEU_PROJECT_ID',
+    storageBucket: 'SEU_STORAGE_BUCKET',
+    messagingSenderId: 'SEU_MESSAGING_SENDER_ID',
+    appId: 'SEU_APP_ID'
+}
+
+const app = initializeApp(firebaseConfig)
+
+export const db = getFirestore(app)
