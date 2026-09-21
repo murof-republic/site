4152a32c79a7471df0aa092d8ef641fb034b7261
Trigger <triggerww@outlook.com>
2026-09-21 20:54:32 -0300

    adição da firebase no site

---
+++ src/firebase/firebase.ts
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDDCrasJrYzGP9EHgdjxUZyNS_zvpMmOQY",
+    authDomain: "murofmee.firebaseapp.com",
+    projectId: "murofmee",
+    storageBucket: "murofmee.firebasestorage.app",
+    messagingSenderId: "865619348662",
+    appId: "1:865619348662:web:501caa2cd230c09f0c6a17"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore(app);
